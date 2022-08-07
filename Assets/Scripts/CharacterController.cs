a5448815e4ce5d0b4ddd0761dd8ba3a686fb9579
Hunter Thornton <venatorgames.info@gmail.com>
2022-08-07 08:45:16 -0500

    Added Player Movement and Boundaries

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    //Custom variable for speed in the Inspector
+    [SerializeField]
+    private float _speed = 4.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //start Player Character at center
+        transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ControlMovement();
+    }
+
+    //Method contains all logic for moving the Player Character
+    void ControlMovement()
+    {
+        //Player Movement controll via Input Manager
+        float _verticalMovement = Input.GetAxis("Vertical");
+        float _horizontalMovement = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.up * _verticalMovement * _speed * Time.deltaTime);
+        transform.Translate(Vector3.right * _horizontalMovement * _speed * Time.deltaTime);
+
+        //player boundaries y axis
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -4.4f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.4f, 0);
+        }
+
+        //player wrap-around x axis
+        if (transform.position.x > 9.4f)
+        {
+            transform.position = new Vector3(-9.4f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -9.4f)
+        {
+            transform.position = new Vector3(9.4f, transform.position.y, 0);
+        }
     }
 }
